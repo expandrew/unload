28d612b5259237eef66962bb346a9970c306f602
Andrew M Westling <a@andrewwestling.com>
2015-06-13 11:08:35 -0700

    add stub for Settings section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
+  get 'settings', to: 'settings#index'
   resources :folders do
     resources :upload_urls
     resources :upload_receipts
