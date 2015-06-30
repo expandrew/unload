ce61a1c6dc65ed3c4db6bfdd1b35c7e5a0d7531a
Andrew M Westling <a@andrewwestling.com>
2015-06-29 23:27:17 -0700

    fix dotenv for production environment

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module Unload
   class Application < Rails::Application
     config.i18n.enforce_available_locales = true
